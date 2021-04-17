590815e84913f516a218322ae4adcdea3f0f8465
wissem <wissemdebech@gmail.com>
2021-04-17 13:15:09 +0200

    editPatientTs

--- src/app/editpatient/editpatient.component.ts
+++ src/app/editpatient/editpatient.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-editpatient',
@@ -8,16 +9,26 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EditpatientComponent implements OnInit {
 patient : any
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { 
+  constructor(public  dialogRef : MatDialogRef <EditpatientComponent>,@Inject(MAT_DIALOG_DATA) public data: any , private _snackBar: MatSnackBar) { 
     this.patient=data;
     this.patient.id=data.id
     this.patient.prenom=data.prenom;
     this.patient.maladie=data.maladie;
     this.patient.nom=data.nom;
+    
   }
 
   ngOnInit(): void {
   }
+  
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+    this.dialogRef.close();
+  }
 
 }
+
 export class FormFieldAppearanceExample {}
